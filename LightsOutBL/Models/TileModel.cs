c8dbe068cfa6c2bcda256b3a22ea348142cb0f30
kwstasarva <kwstasarvanitakis@gmail.com>
2017-10-22 15:22:29 +0200

    TileModel creation. TileModel will be used inside the form and will be passed as a parameter for the Button.Tag

---
+++ LightsOutBL/Models/TileModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LightsOutBL.Models
+{
+    public class TileModel
+    {
+        public int TilePositionX { get; set; }
+        public int TilePositionY { get; set; }
+        public bool TileState { get; set; }
+    }
+}
